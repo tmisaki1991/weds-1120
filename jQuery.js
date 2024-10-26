d21a97cb61c118f1746bb84ad2e5adb6548d033f
weds-241120 <90s.beats.me@gmail.com>
2024-10-27 01:12:40 +0900

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -46,3 +46,16 @@ $(window).on('load scroll',function(){
         }
     });
 });
+
+// スライドインアニメーション
+$(window).scroll(function (){
+    $('.fadein').each(function(){
+        var elemPos = $(this).offset().top,
+        scroll = $(window).scrollTop(),
+        windowHeight = $(window).height();
+
+            if (scroll > elemPos - windowHeight + 150){
+                $(this).addClass('scrollin');
+            }
+    });
+});
