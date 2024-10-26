4664f224e7adabb0a88ea288efe3e257d855eff0
weds-241120 <90s.beats.me@gmail.com>
2024-10-27 01:56:07 +0900

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -45,3 +45,38 @@ $(window).scroll(function (){
             }
     });
 });
+
+
+
+// トップへ戻る
+$(document).ready(function () {
+    $("#top-btn").hide();　//ボタンを非表示にする
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 100) { //ページの上から100pxスクロールした時
+            $("#top-btn").fadeIn("fast"); //ボタンがフェードインする
+        } else {
+            $("#top-btn").fadeOut("fast");　//ボタンがフェードアウトする
+        }
+        scrollHeight = $(document).height(); //ドキュメントの高さ 
+        scrollPosition = $(window).height() + $(window).scrollTop(); //現在地 
+        footHeight = $("footer").innerHeight(); //止めたい位置の高さ(今回はfooter)
+        if (scrollHeight - scrollPosition <= footHeight) { //ドキュメントの高さと現在地の差がfooterの高さ以下の時
+            $("#top-btn").css({
+                "position": "absolute", //pisitionをabsoluteに変更
+            });
+        } else { //それ以外の場合は
+            $("#top-btn").css({
+                "position": "fixed", //固定表示
+            });
+        }
+    });
+ 
+ 
+//スムーススクロール設定
+    $('#top-btn').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);　//スムーススクロールの速度
+        return false;
+    });
+});
