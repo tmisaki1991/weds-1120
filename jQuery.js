596c4f035f939c0381e0f4054239be343df73a01
weds-241120 <90s.beats.me@gmail.com>
2024-10-27 03:00:28 +0900

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -59,11 +59,7 @@ $(document).ready(function () {
         }
         scrollHeight = $(document).height(); //ドキュメントの高さ 
         scrollPosition = $(window).height() + $(window).scrollTop(); //現在地 
-        footHeight = $("footer").innerHeight(); //止めたい位置の高さ(今回はfooter)
-        if (scrollHeight - scrollPosition <= footHeight) { //ドキュメントの高さと現在地の差がfooterの高さ以下の時
-            $("#top-btn").css({
-                "position": "absolute", //pisitionをabsoluteに変更
-            });
+        
         } else { //それ以外の場合は
             $("#top-btn").css({
                 "position": "fixed", //固定表示
